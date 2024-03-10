ef019d3b3dff2fd5c3b7efefda16afa36c51f233
sergio rueda <sergiorueda90@hotmail.com>
2024-03-10 18:01:43 -0500

    Change telegram3

--- vistas/modulos/getwebhookinfo.php
+++ vistas/modulos/getwebhookinfo.php
@@ -2,7 +2,7 @@
 <?php
 
 include "config.php";
-$apiUrl = "https://api.telegram.org/bot{$BOT_TOKEN}/getWebhookinfo";
+$apiUrl = "https://api.telegram.org/bot{$BOT_TOKEN}/getWebhookInfo";
 $response = file_get_contents($apiUrl);
 echo $response;
 ?>
