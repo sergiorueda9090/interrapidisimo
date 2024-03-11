bfb44f03686884f97035f5c9ef4479dca14f2f8c
sergio rueda <sergiorueda90@hotmail.com>
2024-03-10 20:47:08 -0500

    Document.json

--- sendmessage.php
+++ sendmessage.php
@@ -1,46 +1,36 @@
 <?php
-//include "config.php";
-$data = file_get_contents('php://input');
-$logFile = "webhooksenddata.json";
-$log = fopen($logFile, "a");
-fwrite($log, $data);
-fclose($log);
 
+// Token de tu bot de Telegram
+$botToken = '7022150351:AAGhcgNl6YY2CAupmhyXUw4cGcnUSPJk0Vg';
 
-$getData = json_decode($data, true);
-$userId = 2027398274;//$getData['message']['from']['id'];
-$botMessage = "Hi, there tow";
-
-// Construir los parámetros de la solicitud
-$parameters = array(
-    "chat_id" => $userId,
-    "text" => $botMessage,
-    "parse_mode" => "html"
-);
-
-// URL de la API de Telegram para enviar mensajes
-$telegramApiUrl = "https://api.telegram.org/bot7022150351:AAGhcgNl6YY2CAupmhyXUw4cGcnUSPJk0Vg/sendMessage"; // Reemplaza 'TU_TOKEN' con el token de tu bot
-
-// Inicializar cURL
-$curl = curl_init($telegramApiUrl);
-
-// Establecer opciones para la solicitud cURL
-curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-curl_setopt($curl, CURLOPT_POST, true);
-curl_setopt($curl, CURLOPT_POSTFIELDS, $parameters);
-
-// Ejecutar la solicitud cURL y almacenar la respuesta
-$response = curl_exec($curl);
-
-// Verificar si hay errores
-if ($response === false) {
-    $error = curl_error($curl);
-    echo "Error al enviar la solicitud cURL: $error";
-} else {
-    echo "Mensaje enviado correctamente!";
+// Función para enviar un mensaje por Telegram a un usuario específico
+function enviarMensajeTelegram($userId, $mensaje) {
+    global $botToken;
+    $url = "https://api.telegram.org/bot$botToken/sendMessage";
+    $params = array(
+        'chat_id' => $userId,
+        'text' => $mensaje
+    );
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_POST, true);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $params);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    $resultado = curl_exec($ch);
+    curl_close($ch);
+    return $resultado;
 }
 
-// Cerrar la sesión cURL
-curl_close($curl);
+
+$usuariosMensajes = array(
+    '2027398274' => 'Hola, usuario 1. Este es tu mensaje personalizado.',
+    '6350913805' => 'Hola, usuario 2. Este es tu mensaje personalizado.'
+);
+
+// Bucle para enviar los mensajes por Telegram a cada usuario
+foreach ($usuariosMensajes as $userId => $mensaje) {
+    // Enviar el mensaje por Telegram
+    enviarMensajeTelegram($userId, $mensaje);
+}
 
 ?>
